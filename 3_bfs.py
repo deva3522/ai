b23945c66f5900e15348ba7f45ed25678c17fa53
Devaraj V <109087273+deva3522@users.noreply.github.com>
2024-12-02 23:31:41 +0530

    Update 3_bfs.py

--- 3_bfs.py
+++ 3_bfs.py
@@ -1,16 +1,14 @@
 graph = {
-    '5': ['3', '7'],
-    '3': ['2', '4'],
-    '7': ['8'],
-    '2': [],
-    '4': ['8'],
-    '8': []
+  '1': ['2', '3', '4'],
+  '2': ['4', '5'],
+  '3': ['4'],
+  '4': [],
+  '5': []
 }
 
 def bfs(graph, start):
     queue = [start]
     visited = set()
-
     while queue:
         node = queue.pop(0)
         if node not in visited:
@@ -19,4 +17,4 @@ def bfs(graph, start):
             queue.extend(graph[node])
 
 print("Breadth First Search Traversal:")
-bfs(graph, '5')
+bfs(graph, '1')
